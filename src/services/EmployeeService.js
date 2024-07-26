f6febbd669fc80c5b2c7a42e63779d61de964bd1
nileshjambhulkar015 <nileshjambhulkar@gmail.com>
2024-07-26 10:07:08 +0530

    Kpp added

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -27,7 +27,7 @@ class EmployeeService {
 
     getEmployeeDetailsByPaging() {
         if (null != Cookies.get('empId')) {
-            return axios.get(BASE_URL_API+"/employee/search?statusCd=A&page=0&size=20&sort=emp.emp_fname");
+            return axios.get(BASE_URL_API+"/employee/search?statusCd=A&page=0&size=1200&sort=emp.emp_fname");
         } else {
             alert("You need to login first")
             window.location.replace(LOGIN_UI_BASE_URL);
